3d3aba62fb6060955060beb908cccd331e1f9d86
Your Name <lukemiller4779@gmail.com>
2019-08-29 13:19:04 -0400

    parksBranch

--- scripts/buildHTML.js
+++ scripts/buildHTML.js
@@ -1,33 +1,8 @@
 
 
-//   const buildHTMLparks= (parks) => {
-//     const bigOlContainer =document.querySelector("#bigOlContainer")
-//     let buildHTMLparks = ""
-//   for(let i = 0;i<parks.length;i++){
-  
-//      buildHTMLparks += makeParksComponent.buildParksCard(parks[i])
-//      bigOlContainer.innerHTML = buildHTMLparks
-//      console.log(apiManagerParks)
-//   }
- 
-//   }
-  
-// this is the HTML builder to print out the concerts in the DOM vvvvvvvvv
-const buildConcertsHTML = (concerts)=> {
-    let buildConcertsHTML =""
-    const bigOlContainer = document.querySelector(".bigOlContainer")
-    for(let i=0; i<entries.length;i++){
-        buildConcertsHTML+= makeConcertComponent.buildConcertCard(concerts[i])
-        concertLog.innerHTML = buildConcertsHTML
-    //     console.log(apiManagerConcerts)
-    // }    printing to the dom code goes here ^
-}
-// this is the HTML builder for concerts^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-// Html for a single Brewery Name 
-const buildBreweryHtml = { 
-    buildEntryCard: (singleBreweryItem) =>{
-    return `<section>
-   <h3>${singleBreweryItem.name}</h3>
-   </section>`;
-
-  }}
+  const buildHTMLparks=   {
+      buildEntryCard: (singleParkItem)=>{
+          
+          return`<section><h3>${singleParkItem.park_name}</h3>
+          </section>`}}
+//    
--- scripts/parks.js
+++ scripts/parks.js
@@ -1,1 +1,7 @@
-
+// const parksInput = document.querySelector("#parksInput")
+// document.querySelector("#parksSearchButton").addEventListener("click", function (){
+//     if (event.target.id === parks.park_name ){
+// console.log(parks.park_name)
+//     }
+// })
+// console.log(parks)
